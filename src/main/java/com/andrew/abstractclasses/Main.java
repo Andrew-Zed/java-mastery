003f7e3046e61d1282cdfde220a2f6e714aa20b8
Andrew-Zed <andrew.awadyke@gmail.com>
2022-10-28 20:02:40 +0100

    abstract classes implemented

---
+++ src/main/java/com/andrew/abstractclasses/Main.java
@@ -0,0 +1,19 @@
+package com.andrew.abstractclasses;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog("Bingo");
+        dog.breathe();
+        dog.eat();
+
+        Parrot parrot = new Parrot("Nigerian parrot");
+        parrot.breathe();
+        parrot.eat();
+        parrot.fly();
+
+        Penguin penguin = new Penguin("Emperor");
+        penguin.fly();
+
+    }
+}
